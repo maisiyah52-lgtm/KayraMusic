38bfb8151eba1ebf056e85a715387f0ad4dfce8e
maisiyah52-lgtm <maisiyah52@gmail.com>
2026-05-03 22:04:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
+    "",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
